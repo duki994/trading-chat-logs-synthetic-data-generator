a80d9a8dc69e7061ee7564686c614c17b0b7a3c5
Dusan K. <duki994@gmail.com>
2025-01-23 14:54:06 +0100

    run.py: Generate 5000 trading chat logs by default

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from tradeChatGenerator import TradingChatGenerator
 
 tcg = TradingChatGenerator(tradingConfig.get_config())
 
-chats = tcg.generate_trade_chat(25)
+chats = tcg.generate_trade_chat(5000)
 tcg.save_as_json(chats, "synthetic_trading_chats.json")
